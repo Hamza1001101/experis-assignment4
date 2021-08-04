0ddc6d5f6797a8489ae4e4df63f70f975a7a37b8
Hamza Hayd <dado.org@hotmail.com>
2021-08-04 16:25:17 +0200

    added the bank and work funcationalities

--- js/script.js
+++ js/script.js
@@ -1,28 +1,96 @@
-const balance = 250;
+/**
+ * Laptop info
+ */
 
-document.querySelector(".balance-price").innerHTML = balance;
-const outstandingLoan = document.querySelector('.standing-loan')
+const selectLaptops = document.getElementById("laptops");
+const imageRandom = document.getElementById("imageRandom");
+const laptopPrice = document.querySelector(".laptop-price");
+const laptopName = document.querySelector(".text-header");
+const laptopDescription = document.querySelector(".text-content");
 
-let loanBtn = document.querySelector(".btn-loan");
-let loanCounter = 0 
+/**
+ * Bank info
+ */
 
-loanBtn.addEventListener("click", () => {
-  let userInput = prompt("How much loan do you want to take? ");
-  let loan = +userInput;
-  let maxLoan = balance * 2;
-  if(loanCounter>=1) {
-    alert('You cannot take more loan')
+const displayBalance = document.querySelector(".balance-price");
+const outstandingLoan = document.querySelector(".standing-loan");
+
+/**
+ * Work info
+ */
+
+const displayPay = document.querySelector(".pay-amount");
+const btnWork = document.querySelector(".btn-work");
+const btnBank = document.querySelector(".btn-bank");
+const btnLoan = document.querySelector(".btn-loan");
+const btnBuy = document.querySelector('.btn-buy-now')
+
+let DATA = [];
+let payBalance = 0;
+
+let LAPTOP_PRICE = 0
+//let currentBalanceTracker = 0; 
+let currentBalance = 0 
+let totalBalance = 0; 
+/**
+ * * Work & Pay
+ */
+
+const handlePay = () => {
+  payBalance += 100;
+  displayPay.innerText = payBalance;
+};
+
+const handleBank = () => {
+
+  if(payBalance === 0) {
+    alert('work more')
+    return 
+  }
+  currentBalance += payBalance
+  displayBalance.innerText = currentBalance
+  //currentBalanceTracker = payBalance
+
+  payBalance = 0;
+  displayPay.innerText = 0;
+
+};
+
+
+
+const handleLoan = () => {
+  
+  let userInput = prompt('How much loan do you need?')
+  
+  if(userInput === '' || userInput === null) {
+    return alert('Sorry You Need to enter a valid value')
+  }
+  let loan = +userInput
+  let allowedLoan = currentBalance * 2
+  let loansTaken = 0; 
+
+
+  if(loansTaken > 0) {
+    alert('Buy something first')
     return
   }
-  if (loan > maxLoan) {
-    alert(`Ooobs! You cannot get a loan greater than ${maxLoan}`);
-  } else {
-    loanCounter++; 
-    outstandingLoan.innerText =  `Your outstanding Loan ${loan} Kr` //loan; 
+   if(loan > allowedLoan || (isNaN(userInput)) ) {
+    alert('Sorry You cannot take that much loan!')
+  }
+  else {
+    loansTaken++
+    outstandingLoan.innerText = `Your outstanding loan is ${userInput} Kr`
+    //currentBalanceTracker = +userInput + currentBalanceTracker
+    totalBalance = +userInput + currentBalance
   }
   
-})
+}
 
+
+btnWork.addEventListener("click", handlePay);
+btnBank.addEventListener("click", handleBank);
+
+btnLoan.addEventListener('click', handleLoan)
 
 /**
  * Fetch Data and display it.
@@ -39,13 +107,6 @@ const fetchData = async () => {
   }
 };
 
-const selectLaptops = document.getElementById("laptops");
-const imageRandom = document.getElementById("imageRandom");
-const laptopPrice = document.querySelector(".laptop-price");
-const laptopName = document.querySelector(".text-header");
-const laptopDescription = document.querySelector(".text-content");
-
-let DATA = [];
 const getItemNames = async () => {
   DATA = await fetchData();
 
@@ -57,10 +118,6 @@ const getItemNames = async () => {
     selectLaptops.append(element);
   }
 
-  console.log(DATA[0].image);
-  //laptopName.innerText = selectLaptops.options[selectLaptops.selectedIndex].value;
-
-
   selectLaptops.addEventListener("change", handleLaptopContentChange);
 };
 
@@ -68,12 +125,27 @@ const handleLaptopContentChange = (e) => {
   const selectedLaptop = DATA[e.target.selectedIndex];
   laptopPrice.innerText = selectedLaptop.price;
   laptopName.innerText = selectedLaptop.title;
-  laptopDescription.innerText = selectedLaptop.description
-
+  laptopDescription.innerText = selectedLaptop.description;
+  LAPTOP_PRICE = selectedLaptop.price
   //this is where I'm struggling ??
-  imageRandom.src= `https://noroff-komputer-store-api.herokuapp.com/${selectedLaptop.image}` 
-
-  
+  imageRandom.src = `https://noroff-komputer-store-api.herokuapp.com/${selectedLaptop.image}`;
 };
 
 getItemNames();
+
+
+btnBuy.addEventListener('click', () => {
+  if(LAPTOP_PRICE > totalBalance) {
+    alert('Insufficient amount')
+    return 
+  } 
+  else  {
+    totalBalance -= LAPTOP_PRICE
+    currentBalance = currentBalance - totalBalance
+   // currentBalanceTracker= currentBalanceTracker - LAPTOP_PRICE
+
+    alert('You got a new computer')
+    displayBalance.innerText = totalBalance
+    console.log(totalBalance)
+  }
+})
